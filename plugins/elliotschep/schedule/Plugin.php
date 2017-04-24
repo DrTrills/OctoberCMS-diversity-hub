a58e8269f41b2b247037824a23be78dc55906209
_ratskin_ <elliot.schep@gmail.com>
2017-04-24 11:17:32 +0200

    Updates

---
+++ plugins/elliotschep/schedule/Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace ElliotSchep\Schedule;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
---
+++ plugins/elliotschep/schedule/updates/builder_table_create_elliotschep_schedule_.php
@@ -0,0 +1,27 @@
+<?php namespace ElliotSchep\Schedule\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateElliotschepSchedule extends Migration
+{
+    public function up()
+    {
+        Schema::create('elliotschep_schedule_', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('talk_time');
+            $table->text('talk_summary');
+            $table->string('talk_location');
+            $table->string('talk_duration');
+            $table->string('talk_title');
+            $table->string('slug');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('elliotschep_schedule_');
+    }
+}
